eb05e1729ab76647fa131b212ee1440110e66cf8
shunsukw <ww.shunsuke@gmail.com>
2018-08-18 12:19:24 +0900

    in progress

---
+++ work_force_auth/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type JwtToken struct {
+	Token string `json:"token"`
+}
+
+type Exception struct {
+	Message string `json:"message"`
+}
+
+func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request) {
+	var user User
+	_ = json.NewDecoder(req.Body).Decode(&user)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+		"password": user.Password,
+	})
+	tokenString, error := token.SignedString([]byte("secret"))
+	if error != nil {
+		fmt.Println(error)
+	}
+	json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
+}
+
+func main() {
+	router := mux.NewRouter()
+	fmt.Println("Starting the auth application...")
+	router.HandleFunc("/authenticate", CreateTokenEndpoint).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
