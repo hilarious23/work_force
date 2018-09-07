0bdada336696410e28cc92aa539a14b692b580ca
hilarious23 <s.positex11@gmail.com>
2018-09-07 17:11:33 +0900

    getdeployedtaskforce

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,38 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        work_force
-      </h1>
-      <h2 class="subtitle">
-        My extraordinary Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    {{ deployedContract　}}
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Factory from '~/build/contracts/Factory.json'
+import Web3 from 'web3'
 
 export default {
-  components: {
-    Logo
-  }
+    data() {
+      return {
+        deployedContract: ''
+      }
+    },
+    mounted() {
+      this.init()
+    },
+    methods: {
+      async init() {
+        this.web3 = new Web3(window.web3.currentProvider)
+        this.web3.eth.defaultAccount = this.web3.eth.accounts[0]
+        const factoryabi = Factory.abi
+        const factoryaddress = Object.values(Factory.networks)[0].address
+        this.factory = this.web3.eth
+          .contract(factoryabi)
+          .at(factoryaddress)
+        this.factory.getDeployedTaskForces.call((err,res) => {
+          this.deployedContract = res
+        })
+      }
+    }
 }
 </script>
 
 <style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
 </style>
