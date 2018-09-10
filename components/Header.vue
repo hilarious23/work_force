3fc34a31876c58f65c76bf1054b19ff92200bf9c
hilarious23 <s.positex11@gmail.com>
2018-09-10 22:48:35 +0900

    Create Header and Footer

---
+++ components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header class="header">
+        <ul class="nav-items">
+            <li class="home-botton">
+                <nuxt-link to="/">Home</nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<style scoped>
+    .header {
+        width: 100%;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        left: 0px;
+        right: 0px;
+        top: 0px;
+    }
+    .nav-items {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 80%; 
+        background-color: rgb(65, 209, 245);
+    }
+    .nav-items a {
+        text-decoration: none;
+        color: black;
+    }
+    .nav-items a:hover,
+    .nav-items a:active {
+        color: white;
+    }
+</style>
